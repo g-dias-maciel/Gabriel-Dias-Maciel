92f9063b3c54220f35deca392dc98bbc265c8864
Gabriel Dias Maciel <g.dias-maciel@steffenundbach.de>
2022-05-07 21:02:29 +0200

    #Thinksurance
    - database start

---
+++ database/schema.sql
@@ -0,0 +1,32 @@
+
+-- create database
+CREATE DATABASE bookstore;
+USE bookstore;
+
+CREATE TABLE `users` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `user_email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `user_status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `books` (
+  `book_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `book` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `author` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `book_status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `sales` (
+  `sales_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sale_date` datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` bigint(20) unsigned NOT NULL,,
+  `book_id` bigint(20) unsigned NOT NULL,
+  `sale_status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`sales_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
