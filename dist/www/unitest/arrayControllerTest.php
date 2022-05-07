b5753ebfd6f9395c79d932a4da8b8d272f2bcd5d
Gabriel Dias Maciel <g.dias-maciel@steffenundbach.de>
2022-05-07 22:44:43 +0200

    #Thinksurance
    - updated unitest assert functions
    - created new case for number that might or might not be included on array

--- dist/www/unitest/arrayControllerTest.php
+++ dist/www/unitest/arrayControllerTest.php
@@ -53,6 +53,44 @@ class arrayControllerTest extends TestCase
         print "Search result (-1 if not found) else item index: \r\n";
         print_r($actual);
 
-        $this->assertEquals($search_value, $array[$actual]);
+        //if value not found by arraySearch() assert it doesn't cotains the value
+        if ($actual == -1){
+            print "\r\n".'Asserting that the array does not contain the searched value';
+            $this->assertNotContains($search_value, $array);
+        }else{
+            print "\r\n".'Asserting that the array contains the searched value';
+            $this->assertContains($search_value, $array);
+        }
+    }
+
+    /**
+     * test case where we look for a value that might exist or not in the array
+     */
+    public function testCase3()
+    {
+        print "\r\n";
+        print "Running test case - Value included on array \n\r";
+
+        $search_value = 233700;
+
+        $array = arrayController::generateIntArray(10e4, 10e5, 0, 10e5);
+
+        print "\r\n";
+        print "Random array created\r\n";
+        print "Array size: " . count($array) . "\r\n";
+        print "Searching for value: " . $search_value . "\r\n";
+        $actual = arrayController::arraySearch($search_value, $array, count($array), 0);
+
+        print "Search result (-1 if not found) else item index: \r\n";
+        print_r($actual);
+
+        //if value not found by arraySearch() assert it doesn't cotains the value
+        if ($actual == -1){
+            print "\r\n".'Asserting that the array does not contain the searched value';
+            $this->assertNotContains($search_value, $array);
+        }else{
+            print "\r\n".'Asserting that the array contains the searched value';
+            $this->assertContains($search_value, $array);
+        }
     }
 }
